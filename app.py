262614f1426475c879835ce09846b6e051ceb7c4
y4zmin <yaz2003nr@gmail.com>
2026-04-27 10:24:59 -0700

    setting up repo

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import psycopg2
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+conn = psycopg2.connect(
+    host="postgres",
+    database="attendancedb",
+    user="postgres",
+    password="1234"
+)
+
+cur = conn.cursor()
