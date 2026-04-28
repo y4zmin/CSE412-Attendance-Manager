c7f2a58bd4d13cb660794732fdfc16f39751f23c
y4zmin <yaz2003nr@gmail.com>
2026-04-28 12:50:11 -0700

    created database connection via flask + supabase cloud/URL connection

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
+import os
 from flask import Flask, render_template
 import psycopg2
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 app = Flask(__name__)
+
+db_url = "postgresql://postgres:NMxZ2Ov8kz0M78hg@db.mrwaacfrwwyfsjyzwgld.supabase.co:5432/postgres"
+
+def get_db_connection():
+        return psycopg2.connect(db_url)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-
-
 if __name__ == "__main__":
     app.run(debug=True)
-
-conn = psycopg2.connect(
-    host="localhost",
-    database="attendancedb",
-    user="postgres",
-    password="1234"
-)
-
-cur = conn.cursor()
--- attendance_db.sql
+++ attendance_db.sql
@@ -2,12 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict ijRO0CyzNMR3F5uQLIkInLGe4qOHXhDD0Y8LnoSHh4TZNOanaRYm0JGJhnlx09C
+\restrict AClIJk5MY0SPFYzyn1vTrfdVbWG6KvU15ebjSU38SApNVXT7kFgnmafksQW9Wg9
 
--- Dumped from database version 18.1
--- Dumped by pg_dump version 18.1
+-- Dumped from database version 14.20 (Homebrew)
+-- Dumped by pg_dump version 18.0
 
--- Started on 2026-03-27 14:43:11
+-- Started on 2026-04-04 14:04:36 MST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -22,7 +22,17 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 865 (class 1247 OID 16662)
+-- TOC entry 4 (class 2615 OID 2200)
+-- Name: public; Type: SCHEMA; Schema: -; Owner: hrishiresham
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+ALTER SCHEMA public OWNER TO hrishiresham;
+
+--
+-- TOC entry 829 (class 1247 OID 16575)
 -- Name: attendancetype; Type: TYPE; Schema: public; Owner: postgres
 --
 
@@ -37,7 +47,7 @@ CREATE TYPE public.attendancetype AS ENUM (
 ALTER TYPE public.attendancetype OWNER TO postgres;
 
 --
--- TOC entry 862 (class 1247 OID 16656)
+-- TOC entry 832 (class 1247 OID 16584)
 -- Name: classtype; Type: TYPE; Schema: public; Owner: postgres
 --
 
@@ -50,7 +60,7 @@ CREATE TYPE public.classtype AS ENUM (
 ALTER TYPE public.classtype OWNER TO postgres;
 
 --
--- TOC entry 859 (class 1247 OID 16646)
+-- TOC entry 835 (class 1247 OID 16590)
 -- Name: usertype; Type: TYPE; Schema: public; Owner: postgres
 --
 
@@ -69,7 +79,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 226 (class 1259 OID 16729)
+-- TOC entry 209 (class 1259 OID 16599)
 -- Name: attendancerecord; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -86,7 +96,7 @@ CREATE TABLE public.attendancerecord (
 ALTER TABLE public.attendancerecord OWNER TO postgres;
 
 --
--- TOC entry 225 (class 1259 OID 16728)
+-- TOC entry 210 (class 1259 OID 16602)
 -- Name: attendancerecord_attendanceid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -102,8 +112,8 @@ CREATE SEQUENCE public.attendancerecord_attendanceid_seq
 ALTER SEQUENCE public.attendancerecord_attendanceid_seq OWNER TO postgres;
 
 --
--- TOC entry 5063 (class 0 OID 0)
--- Dependencies: 225
+-- TOC entry 3860 (class 0 OID 0)
+-- Dependencies: 210
 -- Name: attendancerecord_attendanceid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -111,7 +121,7 @@ ALTER SEQUENCE public.attendancerecord_attendanceid_seq OWNED BY public.attendan
 
 
 --
--- TOC entry 222 (class 1259 OID 16687)
+-- TOC entry 211 (class 1259 OID 16603)
 -- Name: classes; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -127,7 +137,7 @@ CREATE TABLE public.classes (
 ALTER TABLE public.classes OWNER TO postgres;
 
 --
--- TOC entry 221 (class 1259 OID 16686)
+-- TOC entry 212 (class 1259 OID 16608)
 -- Name: classes_classid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -143,8 +153,8 @@ CREATE SEQUENCE public.classes_classid_seq
 ALTER SEQUENCE public.classes_classid_seq OWNER TO postgres;
 
 --
--- TOC entry 5064 (class 0 OID 0)
--- Dependencies: 221
+-- TOC entry 3861 (class 0 OID 0)
+-- Dependencies: 212
 -- Name: classes_classid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -152,7 +162,7 @@ ALTER SEQUENCE public.classes_classid_seq OWNED BY public.classes.classid;
 
 
 --
--- TOC entry 224 (class 1259 OID 16706)
+-- TOC entry 213 (class 1259 OID 16609)
 -- Name: enrollment; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -167,7 +177,7 @@ CREATE TABLE public.enrollment (
 ALTER TABLE public.enrollment OWNER TO postgres;
 
 --
--- TOC entry 223 (class 1259 OID 16705)
+-- TOC entry 214 (class 1259 OID 16613)
 -- Name: enrollment_enrollmentid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -183,8 +193,8 @@ CREATE SEQUENCE public.enrollment_enrollmentid_seq
 ALTER SEQUENCE public.enrollment_enrollmentid_seq OWNER TO postgres;
 
 --
--- TOC entry 5065 (class 0 OID 0)
--- Dependencies: 223
+-- TOC entry 3862 (class 0 OID 0)
+-- Dependencies: 214
 -- Name: enrollment_enrollmentid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -192,7 +202,7 @@ ALTER SEQUENCE public.enrollment_enrollmentid_seq OWNED BY public.enrollment.enr
 
 
 --
--- TOC entry 220 (class 1259 OID 16672)
+-- TOC entry 215 (class 1259 OID 16614)
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -209,7 +219,7 @@ CREATE TABLE public.users (
 ALTER TABLE public.users OWNER TO postgres;
 
 --
--- TOC entry 219 (class 1259 OID 16671)
+-- TOC entry 216 (class 1259 OID 16617)
 -- Name: users_userid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -225,8 +235,8 @@ CREATE SEQUENCE public.users_userid_seq
 ALTER SEQUENCE public.users_userid_seq OWNER TO postgres;
 
 --
--- TOC entry 5066 (class 0 OID 0)
--- Dependencies: 219
+-- TOC entry 3863 (class 0 OID 0)
+-- Dependencies: 216
 -- Name: users_userid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -234,7 +244,7 @@ ALTER SEQUENCE public.users_userid_seq OWNED BY public.users.userid;
 
 
 --
--- TOC entry 4884 (class 2604 OID 16732)
+-- TOC entry 3684 (class 2604 OID 16618)
 -- Name: attendancerecord attendanceid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -242,7 +252,7 @@ ALTER TABLE ONLY public.attendancerecord ALTER COLUMN attendanceid SET DEFAULT n
 
 
 --
--- TOC entry 4881 (class 2604 OID 16690)
+-- TOC entry 3685 (class 2604 OID 16619)
 -- Name: classes classid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -250,7 +260,7 @@ ALTER TABLE ONLY public.classes ALTER COLUMN classid SET DEFAULT nextval('public
 
 
 --
--- TOC entry 4882 (class 2604 OID 16709)
+-- TOC entry 3686 (class 2604 OID 16620)
 -- Name: enrollment enrollmentid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -258,7 +268,7 @@ ALTER TABLE ONLY public.enrollment ALTER COLUMN enrollmentid SET DEFAULT nextval
 
 
 --
--- TOC entry 4880 (class 2604 OID 16675)
+-- TOC entry 3688 (class 2604 OID 16621)
 -- Name: users userid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -266,84 +276,142 @@ ALTER TABLE ONLY public.users ALTER COLUMN userid SET DEFAULT nextval('public.us
 
 
 --
--- TOC entry 5057 (class 0 OID 16729)
--- Dependencies: 226
+-- TOC entry 3846 (class 0 OID 16599)
+-- Dependencies: 209
 -- Data for Name: attendancerecord; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.attendancerecord (attendanceid, userid, classid, meetdate, attendance, recorder) FROM stdin;
+1	1	1	2026-01-13	present	2
+2	4	1	2026-01-13	present	2
+3	5	1	2026-01-13	late	2
+4	8	1	2026-01-13	present	2
+5	1	1	2026-01-15	absent	2
+6	4	1	2026-01-15	present	2
+7	5	1	2026-01-15	present	2
+8	8	1	2026-01-15	excused	2
+9	4	2	2026-01-14	present	2
+10	5	2	2026-01-14	present	2
+11	6	2	2026-01-14	absent	2
+12	4	2	2026-01-16	late	2
+13	5	2	2026-01-16	present	2
+14	6	2	2026-01-16	present	2
+15	1	3	2026-01-13	present	3
+16	5	3	2026-01-13	present	3
+17	7	3	2026-01-13	late	3
+18	9	3	2026-01-13	present	3
+19	1	3	2026-01-15	present	3
+20	5	3	2026-01-15	absent	3
+21	7	3	2026-01-15	present	3
+22	9	3	2026-01-15	excused	3
+23	6	4	2026-01-14	present	3
+24	7	4	2026-01-14	present	3
+25	8	4	2026-01-14	absent	3
+26	9	4	2026-01-14	present	3
+27	6	4	2026-01-16	present	3
+28	7	4	2026-01-16	late	3
+29	8	4	2026-01-16	present	3
+30	9	4	2026-01-16	present	3
 \.
 
 
 --
--- TOC entry 5053 (class 0 OID 16687)
--- Dependencies: 222
+-- TOC entry 3848 (class 0 OID 16603)
+-- Dependencies: 211
 -- Data for Name: classes; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.classes (classid, classname, classdesc, category, creator) FROM stdin;
+1	CSE412	Database Management	school	2
+2	CSE355	Intro to Theoretical CS	school	2
+3	MAT267	Calculus for Engineers	school	3
+4	PHY121	University Physics I	school	3
 \.
 
 
 --
--- TOC entry 5055 (class 0 OID 16706)
--- Dependencies: 224
+-- TOC entry 3850 (class 0 OID 16609)
+-- Dependencies: 213
 -- Data for Name: enrollment; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.enrollment (enrollmentid, userid, classid, enrolledat) FROM stdin;
+1	1	1	2026-04-04 13:59:39.693774
+2	1	3	2026-04-04 13:59:39.693774
+3	4	1	2026-04-04 13:59:39.693774
+4	4	2	2026-04-04 13:59:39.693774
+5	5	1	2026-04-04 13:59:39.693774
+6	5	2	2026-04-04 13:59:39.693774
+7	5	3	2026-04-04 13:59:39.693774
+8	6	2	2026-04-04 13:59:39.693774
+9	6	4	2026-04-04 13:59:39.693774
+10	7	3	2026-04-04 13:59:39.693774
+11	7	4	2026-04-04 13:59:39.693774
+12	8	1	2026-04-04 13:59:39.693774
+13	8	4	2026-04-04 13:59:39.693774
+14	9	3	2026-04-04 13:59:39.693774
+15	9	4	2026-04-04 13:59:39.693774
 \.
 
 
 --
--- TOC entry 5051 (class 0 OID 16672)
--- Dependencies: 220
+-- TOC entry 3852 (class 0 OID 16614)
+-- Dependencies: 215
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.users (userid, username, firstname, lastname, password, usertype) FROM stdin;
 1	yrodri47	Yazmin	Rodriguez	password	student
+2	jsmith12	Jake	Smith	pass1234	teacher
+3	mlopez09	Maria	Lopez	pass1234	teacher
+4	kpatel55	Kiran	Patel	pass1234	student
+5	ajones88	Aiden	Jones	pass1234	student
+6	tnguyen03	Tina	Nguyen	pass1234	student
+7	bwilson22	Brian	Wilson	pass1234	student
+8	cgarcia77	Carla	Garcia	pass1234	student
+9	dlee19	David	Lee	pass1234	student
+10	ekim45	Emily	Kim	pass1234	student
 \.
 
 
 --
--- TOC entry 5067 (class 0 OID 0)
--- Dependencies: 225
+-- TOC entry 3864 (class 0 OID 0)
+-- Dependencies: 210
 -- Name: attendancerecord_attendanceid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.attendancerecord_attendanceid_seq', 1, false);
+SELECT pg_catalog.setval('public.attendancerecord_attendanceid_seq', 30, true);
 
 
 --
--- TOC entry 5068 (class 0 OID 0)
--- Dependencies: 221
+-- TOC entry 3865 (class 0 OID 0)
+-- Dependencies: 212
 -- Name: classes_classid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.classes_classid_seq', 1, false);
+SELECT pg_catalog.setval('public.classes_classid_seq', 4, true);
 
 
 --
--- TOC entry 5069 (class 0 OID 0)
--- Dependencies: 223
+-- TOC entry 3866 (class 0 OID 0)
+-- Dependencies: 214
 -- Name: enrollment_enrollmentid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.enrollment_enrollmentid_seq', 1, false);
+SELECT pg_catalog.setval('public.enrollment_enrollmentid_seq', 15, true);
 
 
 --
--- TOC entry 5070 (class 0 OID 0)
--- Dependencies: 219
+-- TOC entry 3867 (class 0 OID 0)
+-- Dependencies: 216
 -- Name: users_userid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.users_userid_seq', 1, true);
+SELECT pg_catalog.setval('public.users_userid_seq', 10, true);
 
 
 --
--- TOC entry 4896 (class 2606 OID 16740)
+-- TOC entry 3690 (class 2606 OID 16623)
 -- Name: attendancerecord attendancerecord_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -352,7 +420,7 @@ ALTER TABLE ONLY public.attendancerecord
 
 
 --
--- TOC entry 4890 (class 2606 OID 16699)
+-- TOC entry 3692 (class 2606 OID 16625)
 -- Name: classes classes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -361,7 +429,7 @@ ALTER TABLE ONLY public.classes
 
 
 --
--- TOC entry 4892 (class 2606 OID 16715)
+-- TOC entry 3694 (class 2606 OID 16627)
 -- Name: enrollment enrollment_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -370,7 +438,7 @@ ALTER TABLE ONLY public.enrollment
 
 
 --
--- TOC entry 4894 (class 2606 OID 16717)
+-- TOC entry 3696 (class 2606 OID 16629)
 -- Name: enrollment enrollment_userid_classid_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -379,7 +447,7 @@ ALTER TABLE ONLY public.enrollment
 
 
 --
--- TOC entry 4886 (class 2606 OID 16683)
+-- TOC entry 3698 (class 2606 OID 16631)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -388,7 +456,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4888 (class 2606 OID 16685)
+-- TOC entry 3700 (class 2606 OID 16633)
 -- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -397,7 +465,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- TOC entry 4900 (class 2606 OID 16746)
+-- TOC entry 3701 (class 2606 OID 16634)
 -- Name: attendancerecord attendancerecord_classid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -406,7 +474,7 @@ ALTER TABLE ONLY public.attendancerecord
 
 
 --
--- TOC entry 4901 (class 2606 OID 16751)
+-- TOC entry 3702 (class 2606 OID 16639)
 -- Name: attendancerecord attendancerecord_recorder_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -415,7 +483,7 @@ ALTER TABLE ONLY public.attendancerecord
 
 
 --
--- TOC entry 4902 (class 2606 OID 16741)
+-- TOC entry 3703 (class 2606 OID 16644)
 -- Name: attendancerecord attendancerecord_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -424,7 +492,7 @@ ALTER TABLE ONLY public.attendancerecord
 
 
 --
--- TOC entry 4897 (class 2606 OID 16700)
+-- TOC entry 3704 (class 2606 OID 16649)
 -- Name: classes classes_creator_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -433,7 +501,7 @@ ALTER TABLE ONLY public.classes
 
 
 --
--- TOC entry 4898 (class 2606 OID 16723)
+-- TOC entry 3705 (class 2606 OID 16654)
 -- Name: enrollment enrollment_classid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -442,7 +510,7 @@ ALTER TABLE ONLY public.enrollment
 
 
 --
--- TOC entry 4899 (class 2606 OID 16718)
+-- TOC entry 3706 (class 2606 OID 16659)
 -- Name: enrollment enrollment_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -450,11 +518,21 @@ ALTER TABLE ONLY public.enrollment
     ADD CONSTRAINT enrollment_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(userid) ON DELETE CASCADE;
 
 
--- Completed on 2026-03-27 14:43:11
+--
+-- TOC entry 3859 (class 0 OID 0)
+-- Dependencies: 4
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: hrishiresham
+--
+
+REVOKE USAGE ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+-- Completed on 2026-04-04 14:04:36 MST
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict ijRO0CyzNMR3F5uQLIkInLGe4qOHXhDD0Y8LnoSHh4TZNOanaRYm0JGJhnlx09C
+\unrestrict AClIJk5MY0SPFYzyn1vTrfdVbWG6KvU15ebjSU38SApNVXT7kFgnmafksQW9Wg9
 
